a57804a969bc75cca4dcd5ff0a4db4854dce2e5b
alfredo tapia <alfrtapia@gmail.com>
2019-05-21 21:16:01 -0500

    First Commit

---
+++ main/precache-manifest.32de14f0137fa0da31dfd852937412d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c5247ba0680741eb4dedd3ef468662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984088145cc4a8a031b6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "984088145cc4a8a031b6",
+    "url": "/static/js/main.8b4fda10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8079bc527d4127b55fb368f53b56cb15",
+    "url": "/static/media/03.8079bc52.jpg"
+  },
+  {
+    "revision": "0ea360c5fa94ea0cbbe3d69c2e87d3df",
+    "url": "/static/media/github.0ea360c5.png"
+  },
+  {
+    "revision": "2f2002db4af50d2c1770ed0e05e44482",
+    "url": "/static/media/newsapp.2f2002db.png"
+  }
+]);
